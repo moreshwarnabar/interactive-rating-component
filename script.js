0023128c083a0364fb66e685b71960fd5101e344
moreshwarnabar <mrnabar@gmail.com>
2024-02-01 15:43:54 -0700

    added event listener for li click

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// globals
+let rating = 0;
+
+// event handlers
+function handleLiClick() {
+  rating = this.textContent;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Loaded");
+  const button = document.querySelector("button");
+  const liList = document.querySelectorAll("li");
+
+  liList.forEach((li) => {
+    li.addEventListener("click", handleLiClick);
+  });
+  button.addEventListener("click", () => console.log("submitted"));
 });
